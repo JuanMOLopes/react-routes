83a3520413c5cabd562a1de168db86eaa16c51c7
Ana <anabfariasp@gmail.com>
2025-02-27 14:51:30 -0300

    Erro concertado

--- src/pages/Grupo.jsx
+++ src/pages/Grupo.jsx
@@ -8,8 +8,8 @@ function Grupo (){
     return(
         <div> 
             <Titulo desafio="PROJETO - REACT" logo={Logo}/>
-        <h1> Grupo 2 - Os caçadores de monstros marinhos </h1>
-        <p> Os integrantes do grupo 2 secretamente, caçam monstros marinhos durante as horas vagas! </p>
+        <h1> Grupo 2 - Os caçadores de monstros marinhos 👹👹 </h1>
+        <p> Os integrantes do grupo 2 secretamente, caçam monstros marinhos durante as horas vagas!  </p>
         <img src="" alt="" />
         <p> </p>
         <Footer texto1="@JuanMOLopes" insta={Instagram} texto2="juan.molopes@gmail.com 💻" gmail={Gmail} texto3="O responsável pelo desenvolvimento foi JuanMOLopes.🎉" />
