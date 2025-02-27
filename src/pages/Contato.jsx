a426312faf120f668b35f8a1f3cdfc9ff85bf0de
agatha <agaalifra@gmail.com>
2025-02-27 14:23:08 -0300

    grupo,contato e sobre

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -6,14 +6,14 @@ function Contato (){
     return(
         <div> 
             <Titulo desafio="PROJETO - REACT" logo={Logo}/>
-        <h1> O monstro do lago Ness </h1>
-        <p> O monstro do lago Ness, monstro de Loch Ness, também conhecido simplesmente por Nessie, é um criptídeo aquático que alegadamente foi visto no Loch Ness (Lago Ness), nas Terras Altas da Escócia, no Reino Unido. A sua existência (ou não) continua a suscitar debate entre os cépticos e os crentes, e é um dos mistérios da criptozoologia.[1] O monstro de Loch Ness é descrito como uma espécie de monstro ou réptil gigante marinho, semelhante ao plesiossauro, um sauropterígeo pré-histórico. </p>
+        <h1> Contato </h1>
+        <p> Contrate nossos serviços como caçadores de monstros marinhos. Somos experientes e dedicados a acabar com as bestas em sua piscina! </p>
         <img src="" alt="" />
-        <p> </p>
+        <p> Telefone : (19)40028922 </p>
         <Footer texto1="@JuanMOLopes" insta={Instagram} texto2="juan.molopes@gmail.com 💻" gmail={Gmail} texto3="O responsável pelo desenvolvimento foi JuanMOLopes.🎉" />
 
         </div>
     )
 }
 
-export default Contato
+export default Contato  
